609b4335d2af095bda22e1d4bbf2ba8e013da6e5
geoncheollee <day071211@gmail.com>
2014-11-03 00:55:28 +0900

    Update top2.js

--- assets/js/top2.js
+++ assets/js/top2.js
@@ -1,49 +1,20 @@
-/*! Plugin options and other jQuery stuff */
-
-// dl-menu options
-$(function() {
-  $( '#dl-menu' ).dlmenu({
-    animationClasses : { classin : 'dl-animate-in', classout : 'dl-animate-out' }
-  });
-});
-
-// FitVids options
-$(function() {
-  $("article").fitVids();
-});
-
-$(".close-menu").click(function () {
-  $(".menu").toggleClass("disabled");
-  $(".links").toggleClass("enabled");
-});
-
-$(".about").click(function () {
-  $("#about").css('display','block');
-});
-
-$(".close-about").click(function () {
-  $("#about").css('display','');
-});
-
-// Add lightbox class to all image links
-$("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
-
-// Magnific-Popup options
-$(document).ready(function() {
-  $('.image-popup').magnificPopup({
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-    },
-    image: {
-      tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
-    },
-    removalDelay: 300, // Delay in milliseconds before popup is removed
-    // Class that is added to body when popup is open. 
-    // make it unique to apply your CSS animations just to this exact popup
-    mainClass: 'mfp-fade'
-  });
-});
+<script language=javascript>
+<!--
+if (isNS4) {
+        var divMenu = document["divMenu"];
+        divMenu.top = windows.pageYOffset + 405;
+        divMenu.visibility = "visible";
+        moveRightEdge();
+} else if (isDOM) {
+        var divMenu = getRef('divMenu');
+             divMenu.style.top = (isNS ? window.pageYOffset +405 : document.body.clientHeight + document.body.scrollTop - 45 );
+        if(!isNS) {
+                if( divMenu.style.top > document.body.scrollHeight - 250 ) divMenu.style.top = document.body.scrollHeight - 235;
+                if( divMenu.style.top < 65 ) divMenu.style.top = 65;
+        }
+                        
+        divMenu.style.visibility = "visible";
+        moveRightEdge();
+}
+//-->
+</script> <!--TOP버튼 끝-->
